77f1e63adf46b4e5ac4c3767fc4d248914cad081
Paul Lame <p.lame@icloud.com>
2023-03-11 12:25:18 +0100

    added routing logique

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ const { buffer } = require('stream/consumers');
 const hostname = '127.0.0.1';
 const port = '3000';
 
-function logRequest(req) {
-    const reqMethod = req.method;
-    const reqHeaders = JSON.stringify(req.headers);
-    const reqURL = req.url;
-
-    console.log(`received request;\n method: ${reqMethod}\n url: ${reqURL}\n headers: ${reqHeaders}`);
+function logRequest(method, url, headers, body) {
+    console.log(`received request;\n method: ${method}\n url: ${url}\n headers: ${JSON.stringify(headers)}\n body: ${body}`);
 };
 
 const server = http.createServer((req,res) => {
@@ -19,23 +15,39 @@ const server = http.createServer((req,res) => {
     req.on('error', (err) => {
         // This prints the error message and stack trace to `stderr`.
         console.error(err.stack);
-      }).on('data',(chunk) => {
+        }).on('data',(chunk) => {
         body.push(chunk);
     }).on('end',() => {
         body = Buffer.concat(body).toString();
+        // log the details of the incoming request
+        logRequest(req.method,req.url,req.headers,body);
     });
 
-    logRequest(req); // log the details of the incoming request
-    console.log(`body is: ${body}`);
+
+    // Routing request
+    if(req.method === 'POST' && req.url === '/wordcount') {
+        if(body && req.headers["content-type"] === 'text/plain') {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/plain');
+            res.end(`your text contains xx words`);
+        }
+        else {
+            res.statusCode = 400;
+            res.setHeader('Content-type', 'text/plain');
+            res.end('Body cannot be empty');
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-type', 'text/plain');
+        res.end('Content not found');
+    }
+
+
 
     res.on('error', (err) => {
         console.log(err);
     });
-
-    // setting response
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World!\n');
 });
 
 server.listen(port, hostname, () => {
